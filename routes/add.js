766bd4610a61b27d64cb2a277d13a282be2f0451
Austin Roddick <aroddick@ucsd.edu>
2020-02-06 14:17:46 -0800

    Lab 5 complete!

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,15 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
+exports.addFriend = function(request, response) {   
 	// Your code goes here
- }
+	var newName = request.query.name;
+	var newDescription = request.query.description;
+	newFriend = {
+		"name" : newName,
+		"description" : newDescription,
+		"image" : 'http://lorempixel.com/400/400/people'
+	};
+	console.log(newFriend);
+	response.render('index', data);
+	data.friends.push(newFriend);
+}
